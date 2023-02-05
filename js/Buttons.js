3cbc4f5b1cd6a2986ea73b75732bca4b77690584
konradburdzel <kburdzel0@gmail.com>
2023-02-05 16:28:05 +0100

    Change class Buttons - add value and operation param

--- js/Buttons.js
+++ js/Buttons.js
@@ -1,18 +1,14 @@
 class Buttons {
-    constructor() {
-        document.getElementById('keys').addEventListener('click', this.selectButton.bind(this)); 
-        let _firstValue = '';
-
-        this.selectButton = e => {
-            const target = e.target.textContent;
-            _firstValue = _firstValue + target;
-            // const display = new Display(firstValue, secondValue);
-            console.log(this.firstValue);
-        }
+    constructor(value, operation) {
+        this.value = value;
+        this.operation = operation;
     }
 
-    // selectButton(e) {
-   
-    // }
+    valueButton() {
+        return this.value;
+    }
 
+    operationButton() {
+        return this.operation;
+    }
 }
