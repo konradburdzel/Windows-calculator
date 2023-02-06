5c1a1db005f993e129f04e995b16532b07949ddb
konradburdzel <kburdzel0@gmail.com>
2023-02-06 23:35:01 +0100

    Add display method to class Calculator

--- js/Calculator.js
+++ js/Calculator.js
@@ -1,12 +1,25 @@
 class Calculator {
     constructor() {
         document.querySelector('#keys').addEventListener('click', e => this.startCalculator(e));
+        this.input = document.querySelector('.input-data');
+        this.inputStorage = document.querySelector('.input-storage');
+        this.firstValue = true;
     }
 
     startCalculator(e) {
         const valueButton = e.target.textContent;
-        const operation = e.target.className;
+        const operation = e.target.dataset.operation;
         this.button = new Buttons(valueButton, operation);
+        this.display();
     }
 
+    display() {
+        if (this.button.operation === 'number') {
+            this.input.textContent += this.button.value;
+        };  
+        
+        // displayInputStorage.textContent = this.inputStorage;
+
+        console.log(this.button);
+    }
 }
