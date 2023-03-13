0b6165546df0439c8d80b6d12d7e473095a09099
konradburdzel <kburdzel0@gmail.com>
2023-03-13 01:17:10 +0100

    Add remove bin method

--- js/History.js
+++ js/History.js
@@ -4,28 +4,36 @@ class History {
         this.secondValue = secondValue;
         this.operation = operation;
         this.result = result;
+        this.historyHandle = document.querySelector('.display-history');
+        this.historyElements = document.querySelector('.history-elements');
 
     }
 
     addToHistory() {
         console.log([this.firstValue, this.secondValue, this.operation, this.result]);
-        let historyElements = document.querySelector('.history-elements');
-        if (historyElements.children[0].textContent === "Nie ma jeszcze żadnej historii") {
-            this.changeStyleForElement(historyElements);
+        if (this.historyElements.children[0].textContent === "Nie ma jeszcze żadnej historii") {
+            this.changeStyleForElement();
         };
         // console.log(historyElements.children[0].textContent === "Nie ma jeszcze żadnej historii");
     }
 
-    changeStyleForElement(historyElements) {
-        let historyHandle = document.querySelector('.display-history');
-        historyElements.children[0].textContent = '';
-        historyElements.style.height = 'auto';
-        historyHandle.style.alignItems = 'flex-end';
-        historyHandle.style.paddingTop = '30px';
+    changeStyleForElement() {
+        this.historyElements.children[0].textContent = '';
+        this.historyElements.style.height = 'auto';
+        this.historyHandle.style.alignItems = 'flex-end';
+        this.historyHandle.style.paddingTop = '30px';
         let bin = document.createElement('div');
         bin.classList.toggle('binHandle');
-        // bin.style.backgroundColor = 'red';
         bin.innerHTML = '<iconify-icon class="bin" icon="ri:delete-bin-line"></iconify-icon>';
-        historyHandle.appendChild(bin);
+        bin.addEventListener('click', () => this.deleteHistory());
+        this.historyHandle.appendChild(bin);
+    }
+
+    deleteHistory() {
+        this.historyHandle.style.alignItems = 'flex-start';
+        this.historyHandle.style.paddingTop = '10px';
+        this.historyElements.children[0].textContent = 'Nie ma jeszcze żadnej historii';
+        const bin = document.querySelector('.binHandle');
+        bin.remove();
     }
 }
