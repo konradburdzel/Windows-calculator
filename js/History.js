1ef71de4502882b395c04ded1082f2b02f21f24d
konradburdzel <kburdzel0@gmail.com>
2023-03-15 23:20:48 +0100

    Fix delete history elements

--- js/History.js
+++ js/History.js
@@ -10,7 +10,7 @@ class History {
     }
 
     addToHistory() {
-        console.log([this.firstValue, this.secondValue, this.operation, this.result]);
+        // console.log([this.firstValue, this.secondValue, this.operation, this.result]);
         if (this.historyElements.children[0].textContent === "Nie ma jeszcze żadnej historii") {
             this.changeStyleForElement();
         };
@@ -18,7 +18,10 @@ class History {
     }
 
     changeStyleForElement() {
-        this.historyElements.children[0].textContent = '';
+        // this.historyElements.children[0].textContent = '';
+        this.historyElements.removeChild(this.historyElements.firstChild);
+        // console.log(this.historyElements.firstChild);
+        this.historyElements.firstChild.remove();
         this.historyElements.style.height = 'auto';
         this.historyHandle.style.alignItems = 'flex-end';
         this.historyHandle.style.paddingTop = '30px';
@@ -32,6 +35,12 @@ class History {
     deleteHistory() {
         this.historyHandle.style.alignItems = 'flex-start';
         this.historyHandle.style.paddingTop = '10px';
+        const lengthChildren = [...this.historyElements.children].length;
+        for (let i = 0; i < lengthChildren - 1; i++) {
+            console.log(this.historyElements.firstChild.textContent);
+            this.historyElements.removeChild(this.historyElements.children[0]);
+            console.log(i);
+        }
         this.historyElements.children[0].textContent = 'Nie ma jeszcze żadnej historii';
         const bin = document.querySelector('.binHandle');
         bin.remove();
