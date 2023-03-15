a4244a3f3f2e6df9f2ba61399db896238b4f9717
konradburdzel <kburdzel0@gmail.com>
2023-03-15 23:51:02 +0100

    Fix adding new operation to history

--- js/History.js
+++ js/History.js
@@ -14,19 +14,33 @@ class History {
         if (this.historyElements.children[0].textContent === "Nie ma jeszcze żadnej historii") {
             this.changeStyleForElement();
         };
+        const newHitoryElement = document.createElement('li');
+        newHitoryElement.classList.add('history-element');
+        this.historyElements.insertBefore(newHitoryElement, this.historyElements.children[0]);
+
+        const ulInsideHistoryElement = document.createElement('ul');
+        ulInsideHistoryElement.classList.add('ul-in-history-element');
+        newHitoryElement.appendChild(ulInsideHistoryElement);
+
+        const liElementOperation = document.createElement('li');
+        liElementOperation.classList.add('li-element-operation');
+        liElementOperation.textContent = `${this.firstValue}  ${this.operation}  ${this.secondValue} =`;
+        ulInsideHistoryElement.appendChild(liElementOperation);
+
+        const liElementResult = document.createElement('li');
+        liElementResult.classList.add('li-element-result');
+        liElementResult.textContent = `${this.result}`;
+        ulInsideHistoryElement.appendChild(liElementResult);
         // console.log(historyElements.children[0].textContent === "Nie ma jeszcze żadnej historii");
     }
 
     changeStyleForElement() {
-        // this.historyElements.children[0].textContent = '';
-        this.historyElements.removeChild(this.historyElements.firstChild);
-        // console.log(this.historyElements.firstChild);
-        this.historyElements.firstChild.remove();
+        this.historyElements.children[0].remove();
         this.historyElements.style.height = 'auto';
         this.historyHandle.style.alignItems = 'flex-end';
         this.historyHandle.style.paddingTop = '30px';
         let bin = document.createElement('div');
-        bin.classList.toggle('binHandle');
+        bin.classList.add('binHandle');
         bin.innerHTML = '<iconify-icon class="bin" icon="ri:delete-bin-line"></iconify-icon>';
         bin.addEventListener('click', () => this.deleteHistory());
         this.historyHandle.appendChild(bin);
